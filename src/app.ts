8d2a46dd852bb193b6be6e2103c558a8b38ee68a
atzisb-kozak <alkergoat@gmail.com>
2021-07-16 14:39:44 +0200

    [Feat](injection) add injection into schema

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,12 @@
 import "reflect-metadata";
 import express, { Express } from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import { createConnection } from 'typeorm';
+import { createConnection, useContainer } from 'typeorm';
 import { SachetResolver } from "./database/resolver/SachetResolver";
 import { buildSchema } from "type-graphql";
 import { EchantionnageResolver } from "./database/resolver/EchantionnageResolver";
 import { logger } from './logger';
+import { Container } from "typeorm-typedi-extensions";
 
 /**
  * Initialize GraphQL API Server (Apollo)
@@ -17,25 +18,35 @@ import { logger } from './logger';
  */
 async function startApolloServer(): Promise<{server: ApolloServer, app: Express} | undefined> {
 	try{
+		// Setup Connection and Schema for Postgres Database
+		useContainer(Container);
 		const connection = await createConnection();
 		const schema = await buildSchema({
-			resolvers: [SachetResolver, EchantionnageResolver]
+			resolvers: [SachetResolver, EchantionnageResolver],
+			container: Container,
+			emitSchemaFile: true,
 		});
+
+		// Initialize Express + Apollo GraphQL Server
 		const app = express();
 		const server = new ApolloServer({
 			schema,
-			introspection: process.env.NODE_ENV === 'production',
-			playground: process.env.NODE_ENV === 'production',
+			introspection: true,
+			playground: true,
     });
 		await server.start();
 	
 		server.applyMiddleware({ app });
 	
+		// TODO: change default port .env 
+		// listen port 4000
 		new Promise(resolve => app.listen({ port: 4000 }));
 		logger.info(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 		return { server, app };
+
 	} catch (error) {
-		logger.error(`[App](startApolloServer) : ${error}`);
+		// unexcepted Error at launch
+		logger.error(`[App](startApolloServer) : ${error.message}`);
 	}
 }
 
