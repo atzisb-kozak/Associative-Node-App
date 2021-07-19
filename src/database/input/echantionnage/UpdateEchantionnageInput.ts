7582a80f0ffa34704608601d08319be0a051b9e2
atzisb-kozak <alkergoat@gmail.com>
2021-07-19 08:57:14 +0200

    [Feat](echantillon) add rest of CRUD for ecahntillion

--- src/database/input/echantionnage/UpdateEchantionnageInput.ts
+++ src/database/input/echantionnage/UpdateEchantionnageInput.ts
@@ -1,10 +1,21 @@
+/**
+ * Import Module
+ */
+import { Echantionnage } from '../../entity/echantionnage';
 import { InputType, Field } from 'type-graphql';
 
+/**
+ * Input for update existing Echantionnage's object
+ *
+ * @export
+ * @class UpdateEchantionnageInput
+ * @implements {Partial<Echantionnage>}
+ */
 @InputType()
-export class UpdateEchantionnageInput {
+export class UpdateEchantionnageInput implements Partial<Echantionnage> {
 
 	@Field()
-	poidsGenerated: number
+	poidsGenerated: number;
 
 	@Field()
 	poidsMeasured: number;
--- src/database/resolver/EchantionnageResolver.ts
+++ src/database/resolver/EchantionnageResolver.ts
@@ -1,18 +1,116 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql';
+/**
+ * Import Modules
+ */
+import { UserInputError } from 'apollo-server-express';
+import { Resolver, Query, Mutation, Arg, ID } from 'type-graphql';
+import { Repository } from 'typeorm';
+import { InjectRepository } from 'typeorm-typedi-extensions';
 import { Echantionnage } from '../entity/echantionnage';
 import { CreateEchantionnageInput } from '../input/echantionnage/CreateEchantionnageInput';
+import { UpdateEchantionnageInput } from '../input/echantionnage/UpdateEchantionnageInput';
+import { logger } from '../../logger';
 
+/**
+ * GraphQL Resolvers for Echantionnage's data
+ *
+ * @export
+ * @class EchantionnageResolver
+ */
 @Resolver()
 export class EchantionnageResolver {
+	constructor(
+		@InjectRepository(Echantionnage)
+		private readonly _echantionnageRepository: Repository<Echantionnage>
+	) {}
+
+	/**
+	 * find all Echantionnage data Query
+	 *
+	 * @return {Echantionnage[]}  {Promise<Echantionnage[]>}
+	 * @memberof EchantionnageResolver
+	 */
 	@Query(() => [Echantionnage])
-	echantionnage() {
-		return Echantionnage.find();
+	async echantionnage(): Promise<Echantionnage[]>{
+		return this._echantionnageRepository.find();
 	}
 
+	/**
+	 * Find specific Ecahntionnage with ID Field
+	 *
+	 * @param {number} id
+	 * @return {Sachet | undefined}  {(Promise<Echantionnage | undefined>)}
+	 * @memberof EchantionnageResolver
+	 */
+	@Query(() => Echantionnage)
+	async echantionnageID(
+		@Arg('echantionID', type => ID) id : number): Promise<Echantionnage | undefined> {
+
+		return this._echantionnageRepository.findOne(id);
+	}
+
+	/**
+	 * Create new Ecahntionnage's data to store in database
+	 *
+	 * @param {CreateEchantionnageInput} data
+	 * @return {*} 
+	 * @memberof EchantionnageResolver
+	 */
 	@Mutation(() => Echantionnage)
-	async createEchantionnage(@Arg('echantillon') echantillon: CreateEchantionnageInput) {
-		const echantionnage = Echantionnage.create(echantillon);
+	async createEchantionnage(@Arg('echantillon') data: CreateEchantionnageInput) {
+		const echantionnage = Echantionnage.create(data);
 		await echantionnage.save();
 		return echantionnage;
+	}
+
+	/**
+	 * Update specific Echantionnage's data 
+	 * 
+	 * Raise a UserInputError if data's ID isn't stored in database
+	 *
+	 * @param {number} id
+	 * @param {UpdateEchantionnageInput} data
+	 * @return {*}  {Promise<boolean>}
+	 * @memberof EchantionnageResolver
+	 */
+	@Mutation(() => Boolean)
+	async updateEchantionnage(
+		@Arg('echantionID', type => ID) id: number,
+		@Arg('echantillon') data: UpdateEchantionnageInput
+	): Promise<boolean> {
+
+		try {
+			const nbAffect = (await this._echantionnageRepository.update(id, {...data})).affected;
+			if (nbAffect !== 0){
+				return true;
+			}
+			throw new Error('data wasn\'t stored in database');
+		} catch (error) {
+			logger.error(`[EchantionnageResolver](updateEchantionnage) : ${error.message}`);
+			throw new UserInputError(error.message);
+		}
+	}
+
+	/**
+	 *Update specific Echantionnage's data 
+	 * 
+	 * Raise a UserInputError if data's ID isn't stored in database
+	 *
+	 * @param {number} id
+	 * @return {*}  {Promise<boolean>}
+	 * @memberof EchantionnageResolver
+	 */
+	@Mutation(() => Boolean)
+	async deleteEchantionnage(@Arg('echantionID', type => ID) id: number): Promise<boolean> {
+		try {
+			const nbAffect = (await this._echantionnageRepository.delete(id)).affected;
+			if (nbAffect !== 0) {
+				return true;
+			}
+			throw new Error('data wasn\'t stored in database');
+		}catch (error) {
+			logger.error(`[EchantionnageResolver](deleteEchantionnage) : ${error.message}`);
+			throw new UserInputError(error.message);
+		}
+		
 	}
 }
